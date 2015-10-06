1b3ee1481e4bf4ee3532a2d2292e5c913c92e229
Leandro Bittencourt <leandro.dias.bittencourt@gmail.com>
2015-10-06 00:49:05 -0300

    Inclusão dos routers e controllers

--- config/express.js
+++ config/express.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var home = require('../app/routes/home');
 
 module.exports = function() {
   var app = express();
   //configuração de ambiente
   app.set('port', 3000);
 
+  //middleware
   app.use(express.static('./public)'));
+  //define a View  Engine
+  app.set('view engine', 'ejs');
+  app.set('views', '.app/views');
+
+  home(app);
+
   return app;
 };

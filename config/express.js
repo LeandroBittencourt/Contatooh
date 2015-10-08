068caf469f4c17d74c059854f758a8192d18df36
Leandro Dias Bittencourt Silva <leandro.dias.bittencourt@gmail.com>
2015-10-08 12:54:03 -0300

    inserido os middlewares bodyParser e Method-override
    Fim do capítulo 2!

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
   var app = express();
@@ -10,7 +11,12 @@ module.exports = function() {
   app.use(express.static('./public'));
   //define a View  Engine
   app.set('view engine', 'ejs');
+  //define a localização das views
   app.set('views','./app/views');
+  //Middlewares
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
 
   load('models', {cwd:'app'})
     .then('controllers')

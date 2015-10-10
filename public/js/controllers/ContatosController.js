c2a0739260ae509fed2c69823d205b49f83f8bbc
Leandro Bittencourt <leandro.dias.bittencourt@gmail.com>
2015-10-10 14:21:43 -0300

    Incluindo o Boostrap e filtro na lista
    Ta ficando bonito!

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -5,4 +5,19 @@ angular.module('contatooh').controller('ContatosController',
     $scope.incrementa =function(){
       $scope.total++;
     };
+  $scope.contatos = [
+    {
+      _id: 1, nome: 'Contato Exemplo 1',
+     email: 'cont1@empresa.com.br'
+    },
+    {
+      _id: 2, nome: 'Contato Exemplo 2',
+     email: 'cont2@empresa.com.br'
+    },
+    {
+      _id: 3, nome: 'Contato Exemplo 3',
+     email: 'cont3@empresa.com.br'
+    }
+  ];
+  $scope.filtro ='';
 });

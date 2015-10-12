fdfe73c4b3096d095bc4eb6524249da74a377680
Leandro Bittencourt <leandro.dias.bittencourt@gmail.com>
2015-10-12 16:24:54 -0300

    Incluido o atualiza contato e insere novo contato

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -8,23 +8,24 @@ var contatos = [
 ];
 
 module.exports = function(){
-    var controller = {};
+  var controller = {};
+  var ID_CONTATO_INC = 3;
 
-    controller.listaContatos = function(req, res){
-        res.json(contatos);
-    };
+  controller.listaContatos = function(req, res){
+      res.json(contatos);
+  };
 
-    controller.obtemContato = function(req, res) {
+  controller.obtemContato = function(req, res) {
 
-        var idContato = req.params.id;
-        var contato = contatos.filter(function(contato){
-            return contato._id==idContato;
-        })[0];
-        contato ?
-            res.json(contato) :
-            res.status(404).send('Contato não encontrado');
-        console.log('Contato ID: ' + req.params.id);
-    };
+      var idContato = req.params.id;
+      var contato = contatos.filter(function(contato){
+          return contato._id==idContato;
+      })[0];
+      contato ?
+          res.json(contato) :
+          res.status(404).send('Contato não encontrado');
+      console.log('Contato ID: ' + req.params.id);
+  };
   
   controller.removeContato = function (req, res){
     var idContato = req.params.id;
@@ -34,5 +35,30 @@ module.exports = function(){
     });
     res.status(204).end();
   };
+  
+  controller.salvaContato = function (req, res){
+    var contato = req.body;
+    contato = contato._id ?
+        atualiza(contato) :
+        adiciona(contato);
+    res.json(contato);
+  };
+  
+  function adiciona(contatoNovo){
+    contatoNovo._id = ++ID_CONTATO_INC;
+    contatos.push(contatoNovo);
+    return contatoNovo;
+  }
+  
+  function atualiza(contatoAlterar){
+    contatos = contatos.map(function(contato){
+      if(contato._id == contatoAlterar._id){
+        contato = contatoAlterar;
+      }
+      return contato;
+    });
+    return contatoAlterar;
+  }
+  
     return controller;
 }
